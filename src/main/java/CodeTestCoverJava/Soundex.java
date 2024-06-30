2b73e6f6774fd64eaa0b70f479e650f8197c47ee
Rahul Jain V <38587824+vrahul1998@users.noreply.github.com>
2024-06-30 16:49:14 +0530

    Breaking down the function more

--- src/main/java/CodeTestCoverJava/Soundex.java
+++ src/main/java/CodeTestCoverJava/Soundex.java
@@ -32,52 +32,51 @@ public class Soundex {
         }
     };
 
-    private static final List<Character> vowels = Arrays.asList('a','e','i','o','u');
-    private static final List<Character> ignoreAppendCharacters = Arrays.asList('w','h','y');
-
+    private static final List<Character> vowels = Arrays.asList('a', 'e', 'i', 'o', 'u');
+    private static final List<Character> ignoreAppendCharacters = Arrays.asList('w', 'h', 'y');
 
 
     public static final char INVALID_CHAR = '8';
+
     public static String generateSoundex(String name) {
         if (name == null || name.isEmpty()) {
             return "";
         }
 
         // Retain the first letter of the name and convert it to uppercase
-        char firstLetter = Character.toUpperCase(name.charAt(0));
-        StringBuilder soundex = new StringBuilder().append(Character.toUpperCase(firstLetter));
-
-        // Convert the rest of the name to uppercase and map to Soundex digits
-        for (int i = 1; i < name.length(); i++) {
-            AppendToStringBuilder(soundex, GetSoundexCharacter(name,i));
-        }
+        StringBuilder soundex = CreateSoundexString(name);
 
         while (soundex.length() < 4) {
             soundex.append('0');
         }
 
-        return soundex.substring(0,4);
+        return soundex.substring(0, 4);
     }
 
-    private static void AppendToStringBuilder(StringBuilder soundex, char c) {
-        if (INVALID_CHAR != c) {
-            soundex.append(c);
+    private static StringBuilder CreateSoundexString(String name) {
+        char firstLetter = Character.toUpperCase(name.charAt(0));
+        StringBuilder soundex = new StringBuilder().append(Character.toUpperCase(firstLetter));
+
+        // Convert the rest of the name to uppercase and map to Soundex digits
+        for (int i = 1; i < name.length(); i++) {
+            char code = GetSoundexCharacter(name, i);
+            if (INVALID_CHAR != code) {
+                soundex.append(code);
+            }
         }
+        return soundex;
+
     }
 
     private static char GetSoundexCharacter(String name, int i) {
         char current = getSoundexCode(name.charAt(i));
-        char previous = getSoundexCode(name.charAt(i-1));
-        if (vowels.contains(name.charAt(i))) {
+        char previous = getSoundexCode(name.charAt(i - 1));
+        if (vowels.contains(name.charAt(i)) ||
+                ignoreAppendCharacters.contains(name.charAt(i)) ||
+                current == previous) {
             return INVALID_CHAR;
         }
-        if (ignoreAppendCharacters.contains(name.charAt(i))) {
-            return INVALID_CHAR;
-        }
-        if (current == previous) {
-            return INVALID_CHAR;
-        }
-        if (vowels.contains(name.charAt(i-1))) {
+        if (vowels.contains(name.charAt(i - 1))) {
             return current;
         }
         if (RepeatedCharactersBetweenIgnoreCharacters(name, i)) {
@@ -91,13 +90,12 @@ public class Soundex {
         if (currentIndex <= 1) {
             return true;
         }
-        if (ignoreAppendCharacters.contains(name.charAt(currentIndex-1))) {
-            return !(getSoundexCode(name.charAt(currentIndex)) == getSoundexCode(name.charAt(currentIndex-2)));
+        if (ignoreAppendCharacters.contains(name.charAt(currentIndex - 1))) {
+            return !(getSoundexCode(name.charAt(currentIndex)) == getSoundexCode(name.charAt(currentIndex - 2)));
         } else {
             return true;
         }
     }
-
 
 
     private static char getSoundexCode(char c) {
