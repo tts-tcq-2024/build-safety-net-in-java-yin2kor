6b578386ae23ce792d0f3da9454c4cdacd48c24b
Rahul Jain V <38587824+vrahul1998@users.noreply.github.com>
2024-06-30 15:32:00 +0530

    Created pure function to get character

--- src/main/java/CodeTestCoverJava/Soundex.java
+++ src/main/java/CodeTestCoverJava/Soundex.java
@@ -2,34 +2,49 @@ package CodeTestCoverJava;
 
 public class Soundex {
 
+    public static final char INVALID_CHAR = '8';
     public static String generateSoundex(String name) {
-        String result = "";
-        if (null != name && !name.isEmpty()) {
-            StringBuilder soundex = new StringBuilder("0000");
-            int index = 0;
-            soundex.setCharAt(index++,Character.toUpperCase(name.charAt(0)));
-            char prevCode = getSoundexCode(name.charAt(0));
-            boolean canAppend = false;
-            for (int i = 1; i < name.length() && index < 4; i++) {
-                char code = getSoundexCode(name.charAt(i));
-                if (code == '0') {
-                    canAppend = true;
-                    continue;
-                }
-                if (code == '7') {
-                    prevCode = code;
-                    continue;
-                }
-                if (canAppend || code != prevCode) {
-                    soundex.setCharAt(index++,code);
-                }
-                prevCode = code;
-                canAppend = false;
-            }
-            result = soundex.toString();
+        if (name == null || name.isEmpty()) {
+            return "";
         }
 
+        // Retain the first letter of the name and convert it to uppercase
+        char firstLetter = Character.toUpperCase(name.charAt(0));
+        StringBuilder soundex = new StringBuilder().append(firstLetter);
+
+        // Convert the rest of the name to uppercase and map to Soundex digits
+        for (int i = 1, index = 1; i < name.length() && index < 4; i++) {
+            char code = getSoundexCode(name.charAt(i));
+            if (code != '0' && code != '7') {
+                char currentCode = GetSoundexCharacter(name, i);
+                if (INVALID_CHAR != currentCode)
+                {
+                    soundex.append(currentCode);
+                    index++;
+                }
+            }
+        }
+
+        // Pad with '0' to ensure the code is 4 characters long
+        while (soundex.length() < 4) {
+            soundex.append('0');
+        }
+
+        return soundex.toString();
+    }
+
+    private static char GetSoundexCharacter(String name, int i) {
+        char result = getSoundexCode(name.charAt(i));
+        if (getSoundexCode(name.charAt(i-1)) == result) {
+            return INVALID_CHAR;
+        }
+        if (i > 1 && getSoundexCode(name.charAt(i-1)) == '7') {
+            if (getSoundexCode(name.charAt(i-2)) == result) {
+                return INVALID_CHAR;
+            }
+        }
         return result;
+
     }
 
 
