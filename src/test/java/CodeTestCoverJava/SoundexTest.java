8a0946d26833eed4a21dfb187328b3b5b51e4b94
Rahul Jain V <38587824+vrahul1998@users.noreply.github.com>
2024-07-06 16:00:59 +0530

    Used parameterized test case

--- src/test/java/CodeTestCoverJava/SoundexTest.java
+++ src/test/java/CodeTestCoverJava/SoundexTest.java
@@ -4,65 +4,37 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 public class SoundexTest {
 
-    @Test
-    public void testEmptyString() {
-        assertEquals(Soundex.generateSoundex(""),"");
-    }
-
-    @Test
-    public void testNullString() {
-        assertEquals(Soundex.generateSoundex(null), "");
-    }
-
-    @Test
-    public void testSingleCharacter() {
-         assertEquals(Soundex.generateSoundex("A"),"A000");
-    }
-
-    @Test
-    public void testTwoCharacters() {
-         assertEquals(Soundex.generateSoundex("AN"),"A500");
-    }
-
-    @Test
-    public void testThreeCharacters() {
-         assertEquals(Soundex.generateSoundex("AMP"),"A510");
-    }
-
-    @Test
-    public void testFourCharacters() {
-         assertEquals(Soundex.generateSoundex("HELP"),"H410");
-    }
-
-    @Test
-    public void testConsecutiveSameCodeCharacter() {
-         assertEquals(Soundex.generateSoundex("Jackson"),"J250");
-    }
-
-    @Test
-    public void testRepeatedSameCodeDifferentCharacters() {
-         assertEquals(Soundex.generateSoundex("Pfister"),"P236");
-    }
-
-    @Test
-    public void testConsecutiveCharRepeatedSameCodeDifferentChar() {
-         assertEquals(Soundex.generateSoundex("Gutierrez"),"G362");
-    }
-
-    @Test
-    public void testConsecutiveCharRepeatedSameCodeDifferentChar2() {
-         assertEquals(Soundex.generateSoundex("Honeyman"),"H555");
-    }
-
-    @Test
-    public void testInvalidCharacters() {
-        assertEquals(Soundex.generateSoundex("R@#*("),"R000");
+    @ParameterizedTest
+    @MethodSource("provideStringsForGenerateSoundex")
+    public void testWords(String word, String actualCode) {
+        Assertions.assertEquals(Soundex.generateSoundex(word), actualCode);
     }
 
 
+    private static Stream<Arguments> provideStringsForGenerateSoundex() {
+        return Stream.of(
+                Arguments.of("", ""),
+                Arguments.of(null, ""),
+                Arguments.of("A", "A000"),
+                Arguments.of("AN", "A500"),
+                Arguments.of("AMP", "A510"),
+                Arguments.of("HELP", "H410"),
+                Arguments.of("Jackson", "J250"),
+                Arguments.of("Pfister", "P236"),
+                Arguments.of("Honeyman", "H555"),
+                Arguments.of("R@#*(", "R000")
+        );
+    }
 
 
 }
